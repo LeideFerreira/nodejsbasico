077f560b0fd85b5677e35ea0f2bf7beebcefc6cf
LeideFerreira <lmsf@icomp.ufam.edu.br>
2020-05-18 16:54:17 -0400

    CRUD da Api completa

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const requireDir = require('require-dir');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
-
+app.use(express.json());
 // iniciando o DB
+app.use(cors()); //acessar api puublicamente nao entendi ainda vou pesquisar melhor
 mongoose.connect('mongodb://localhost:27017/nodeapi',
 {
     useNewUrlParser:true,
--- app/controllers/ProductController.js
+++ app/controllers/ProductController.js
@@ -2,8 +2,26 @@ const mongoose = require('mongoose');
 const Product = mongoose.model("Product");
 
 const index = async (req,res)=>{
-    const Products = await Product.find();
-    return res.json(Products);
+    const {page = 1} = req.query;
+    const product = await Product.paginate({},{page:page,limit:10});
+    return res.json(product);
+};
+const store = async(req,res)=>{
+    const product = await Product.create(req.body);//criar um produto a partir do json enviado pelo insomnia
+    return res.json(product);
 };
 
-module.exports = {index}
+const show = async(req,res)=>{
+    const product = await Product.findById(req.params.id);
+    return res.json(product);
+};
+
+const update = async(req,res)=>{
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body,{new: true});
+    return res.json(product);
+};
+const destroy = async(req,res)=>{
+    const product = await Product.findByIdAndRemove(req.params.id);
+    res.send();
+};
+module.exports = {index,store,show,update,destroy}
